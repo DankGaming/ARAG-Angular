22a3ae3f4974b3b9c36770f9ac39103f2d439654
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-12-09 14:26:52 +0100

    The right navigationview is visible when logged in vs logged out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "./auth/auth.service";
+import { LoginInfo } from "./auth/login-info.model";
 
 @Component({
 	selector: "app-root",
@@ -7,9 +8,14 @@ import { AuthService } from "./auth/auth.service";
 	styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
+	isLoggedIn: boolean = false;
+
 	constructor(private authService: AuthService) {}
 
 	ngOnInit(): void {
 		this.authService.autoLogin();
+		this.authService.loginInfo.subscribe(
+			(loginInfo: LoginInfo) => (this.isLoggedIn = loginInfo != null)
+		);
 	}
 }
