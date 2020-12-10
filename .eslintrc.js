936b44c489b8148b59f6b632958760d7ec5eac10
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-12-10 15:21:07 +0100

    Deleted comment in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,6 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking"
     ],
-    // "parser": "@typescript-eslint/parser",
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "project": ["./tsconfig.json"],
