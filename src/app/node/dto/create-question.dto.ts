9cc54b0b636d9e450a75731f5199b07ac8fd7bed
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-12-17 11:47:31 +0100

    Changes

--- src/app/node/dto/create-question.dto.ts
+++ src/app/node/dto/create-question.dto.ts
@@ -2,5 +2,6 @@ import { QuestionInfo } from "../question-info.model";
 import { CreateNodeDTO } from "./create-node.dto";
 
 export class CreateQuestionDTO extends CreateNodeDTO {
+	root?: boolean;
 	info: QuestionInfo;
 }
