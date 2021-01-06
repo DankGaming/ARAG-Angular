79941bd588562b57b8d399dc54423f64aadf31b1
Bastiaan Jansen <Bastiaanj7@outlook.com>
2021-01-06 16:48:04 +0100

    code cleanup

--- src/app/node/modals/link-modal/link-modal.component.ts
+++ src/app/node/modals/link-modal/link-modal.component.ts
@@ -66,13 +66,7 @@ export class LinkModalComponent implements OnInit, Modal {
 
 						if (node.children?.length > 0) {
 							const nodeType: ContentType = node.children[0].type;
-							this.type = {
-								name:
-									nodeType === ContentType.QUESTION
-										? "Vraag"
-										: "Notificatie",
-								value: nodeType,
-							};
+							this.type = this.types.find(type => type.value === nodeType);
 						}
 
 						this.switchType();
