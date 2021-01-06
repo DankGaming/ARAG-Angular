86d46f5239cde664dcfc11ab36436e27263236ba
Bastiaan Jansen <Bastiaanj7@outlook.com>
2021-01-06 12:19:15 +0100

    space

--- src/app/shared/modals/confirm-box-modal/confirm-box-modal.component.ts
+++ src/app/shared/modals/confirm-box-modal/confirm-box-modal.component.ts
@@ -2,29 +2,28 @@ import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { Modal } from "../modal.interface";
 
 @Component({
-  selector: "app-confirm-box-modal",
-  templateUrl: "./confirm-box-modal.component.html",
-  styleUrls: ["./confirm-box-modal.component.scss"]
+	selector: "app-confirm-box-modal",
+	templateUrl: "./confirm-box-modal.component.html",
+	styleUrls: ["./confirm-box-modal.component.scss"],
 })
 export class ConfirmBoxModalComponent implements OnInit, Modal {
-  @Input() title: string = "Weet u het zeker?";
-  @Output() closeModal = new EventEmitter();
-  @Output() confirmed = new EventEmitter();
-  @Output() rejected = new EventEmitter();
-  constructor() { }
+	@Input() title: string = "Weet u het zeker?";
+	@Output() closeModal = new EventEmitter();
+	@Output() confirmed = new EventEmitter();
+	@Output() rejected = new EventEmitter();
+	constructor() {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
 	close = (): void => this.closeModal.emit();
 
-  confirm(): void {
-    this.close();
-    this.confirmed.emit();
-  }
+	confirm(): void {
+		this.close();
+		this.confirmed.emit();
+	}
 
-  reject(): void  {
-    this.close();
-    this.rejected.emit();
-  }
+	reject(): void {
+		this.close();
+		this.rejected.emit();
+	}
 }
