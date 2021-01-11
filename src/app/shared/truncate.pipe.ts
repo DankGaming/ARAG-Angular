538e24f5ca7468c439e7503d3817589e22f17824
Bastiaan Jansen <Bastiaanj7@outlook.com>
2021-01-11 11:18:03 +0100

    fixed linting issues

--- src/app/shared/truncate.pipe.ts
+++ src/app/shared/truncate.pipe.ts
@@ -4,9 +4,9 @@ import { Pipe, PipeTransform } from "@angular/core";
 	name: "truncate",
 })
 export class TruncatePipe implements PipeTransform {
-	transform(value: any, characters: number) {
+	transform(value: string, characters: number): string {
 		return value.length <= characters
 			? value
-			: value.substr(0, characters) + "...";
+			: `${value.substr(0, characters)}...`;
 	}
 }
