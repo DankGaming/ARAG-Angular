2643d7e3b6cb1382758e0197dce2a4a6ed147a4c
Bastiaan Jansen <Bastiaanj7@outlook.com>
2021-01-25 10:15:14 +0100

    fixed auto-logout bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,30 +12,31 @@ import { APIInterceptor } from "./shared/api.interceptor";
 import { FormsModule } from "@angular/forms";
 import { FormModule } from "./form/form.module";
 import { EmployeeModule } from "./employee/employee.module";
+import { TestComponent } from "./shared/test/test.component";
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FontAwesomeModule,
-        HttpClientModule,
-        AuthModule,
-        SharedModule,
-        TreeModule,
-        FormsModule,
-        FormModule,
-        EmployeeModule
-    ],
-    providers: [
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: APIInterceptor,
-            multi: true,
-        },
-    ],
-    bootstrap: [AppComponent],
+	declarations: [AppComponent, TestComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FontAwesomeModule,
+		HttpClientModule,
+		AuthModule,
+		SharedModule,
+		TreeModule,
+		FormsModule,
+		FormModule,
+		EmployeeModule,
+	],
+	providers: [
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: APIInterceptor,
+			multi: true,
+		},
+	],
+	bootstrap: [AppComponent],
 })
 export class AppModule {
-    constructor() {}
+	constructor() {}
 }
--- src/app/shared/api.interceptor.ts
+++ src/app/shared/api.interceptor.ts
@@ -1,22 +1,23 @@
 import { Injectable } from "@angular/core";
 import {
-    HttpEvent,
-    HttpInterceptor,
-    HttpHandler,
-    HttpRequest,
+	HttpEvent,
+	HttpInterceptor,
+	HttpHandler,
+	HttpRequest,
 } from "@angular/common/http";
 import { Observable } from "rxjs";
 
 @Injectable()
 export class APIInterceptor implements HttpInterceptor {
-    intercept(
-        req: HttpRequest<any>,
-        next: HttpHandler
-    ): Observable<HttpEvent<any>> {
-        const apiReq = req.clone({
-            url: `http://134.122.55.77:5000${req.url}`,
-        });
+	intercept(
+		req: HttpRequest<any>,
+		next: HttpHandler
+	): Observable<HttpEvent<any>> {
+		const apiReq = req.clone({
+			url: `http://134.122.55.77:5000${req.url}`,
+			// url: `http://localhost:3000${req.url}`,
+		});
 
-        return next.handle(apiReq);
-    }
+		return next.handle(apiReq);
+	}
 }
