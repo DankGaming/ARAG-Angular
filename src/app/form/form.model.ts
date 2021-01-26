b54e5172637cfbd4480e6e2f339a55a0741fe606
addmolition <damienhof27@live.com>
2021-01-26 20:26:05 +0100

    description is nu optional

--- src/app/form/form.model.ts
+++ src/app/form/form.model.ts
@@ -3,8 +3,8 @@ import { FormInput } from "./input/form-input.model";
 export interface Form {
     id: number;
     name: string;
-    description: string;
-    inputs: FormInput[];
+    description?: string;
+    inputs?: FormInput[];
     createdAt: Date;
     updatedAt: Date;
 }
