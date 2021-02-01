4ee45de960b30ff74ee33e1dc58b4899ad490170
Bastiaan Jansen <Bastiaanj7@outlook.com>
2021-02-01 10:48:55 +0100

    file upload styling

--- src/app/form/customer-form-view/customer-form-view.component.ts
+++ src/app/form/customer-form-view/customer-form-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, ViewChild } from "@angular/core";
-import { ActivatedRoute} from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
 import { Form } from "../form.model";
 import { FormService } from "../form.service";
 import { NgForm } from "@angular/forms";
@@ -10,78 +10,91 @@ import { AlertBoxModalComponent } from "src/app/shared/modals/alert-box-modal/al
 import { FormInput } from "../../form/input/form-input.model";
 
 @Component({
-    selector: "app-customer-form-view",
-    templateUrl: "./customer-form-view.component.html",
-    styleUrls: ["./customer-form-view.component.scss"],
+	selector: "app-customer-form-view",
+	templateUrl: "./customer-form-view.component.html",
+	styleUrls: ["./customer-form-view.component.scss"],
 })
 export class CustomerFormViewComponent implements OnInit {
-    @Input() routerLink: any[];
+	@Input() routerLink: any[];
 
-    @ViewChild(PlaceholderDirective, { static: false })
-    modalHost: PlaceholderDirective;
+	@ViewChild(PlaceholderDirective, { static: false })
+	modalHost: PlaceholderDirective;
 
-    form: Form;
-    previousAnswers: {[question: number]: number};
-    attachments: { [name: string]: File } = {};
+	form: Form;
+	previousAnswers: { [question: number]: number };
+	attachments: { [name: string]: File } = {};
 
-    constructor(
-        private activatedRoute: ActivatedRoute,
-        private formService: FormService,
-        private modalService: ModalService,
-        private authService: AuthService) {}
+	constructor(
+		private activatedRoute: ActivatedRoute,
+		private formService: FormService,
+		private modalService: ModalService,
+		private authService: AuthService
+	) {}
 
-    ngOnInit(): void {
-        const queryParams = this.activatedRoute.snapshot.queryParams;
-        const params = this.activatedRoute.snapshot.params;
+	ngOnInit(): void {
+		const queryParams = this.activatedRoute.snapshot.queryParams;
+		const params = this.activatedRoute.snapshot.params;
 
-        this.previousAnswers = JSON.parse(atob(queryParams.answers));
-        this.formService.findByID(+params.id).subscribe((form: Form) => {
-            this.form = form;
-        });
-    }
+		this.previousAnswers = JSON.parse(atob(queryParams.answers));
+		this.formService.findByID(+params.id).subscribe((form: Form) => {
+			this.form = form;
+		});
+	}
 
-    submitAnswers(form: NgForm): void {
-        if (this.authService.isLoggedIn()) {
+	submitAnswers(form: NgForm): void {
+		if (this.authService.isLoggedIn()) {
 			const modal = this.modalService.createModal(
 				AlertBoxModalComponent,
 				this.modalHost
 			);
 			modal.instance.title = "Gelukt!";
-			modal.instance.subtitle = "U bent ingelogd dus er wordt geen mail verzonden";
+			modal.instance.subtitle =
+				"U bent ingelogd dus er wordt geen mail verzonden";
 			return;
 		}
-		if (this.form.inputs.filter((input: FormInput) => input.type.name === "FILE").length !== Object.keys(this.attachments).length) {
+		if (
+			this.form.inputs.filter(
+				(input: FormInput) => input.type.name === "FILE"
+			).length !== Object.keys(this.attachments).length
+		) {
 			const modal = this.modalService.createModal(
 				AlertBoxModalComponent,
 				this.modalHost
 			);
 			modal.instance.title = "Er mist een bestand.";
-			modal.instance.subtitle = "Controleer of u alles juist heeft ingevoerd";
+			modal.instance.subtitle =
+				"Controleer of u alles juist heeft ingevoerd";
 			return;
 		}
 
-		this.formService.submit(this.form.id, {
-			answers: this.previousAnswers,
-			form: form.value,
-			attachments: this.attachments
-		}).subscribe(() => {
-			const modal = this.modalService.createModal(
-				AlertBoxModalComponent,
-				this.modalHost
+		this.formService
+			.submit(this.form.id, {
+				answers: this.previousAnswers,
+				form: form.value,
+				attachments: this.attachments,
+			})
+			.subscribe(
+				() => {
+					const modal = this.modalService.createModal(
+						AlertBoxModalComponent,
+						this.modalHost
+					);
+					modal.instance.title = "Melding verzonden!";
+					modal.instance.subtitle = "We gaan voor u aan de slag.";
+				},
+				() => {
+					const modal = this.modalService.createModal(
+						AlertBoxModalComponent,
+						this.modalHost
+					);
+					modal.instance.title = "Er is een fout opgetreden.";
+					modal.instance.subtitle =
+						"Wellicht heeft u een veld niet ingevoerd.";
+				}
 			);
-			modal.instance.title = "Melding verzonden!";
-			modal.instance.subtitle = "We gaan voor u aan de slag.";
-		}, () => {
-			const modal = this.modalService.createModal(
-				AlertBoxModalComponent,
-				this.modalHost
-			);
-			modal.instance.title = "Er is een fout opgetreden.";
-			modal.instance.subtitle = "Wellicht heeft u een veld niet ingevoerd.";
-		});
-    }
+	}
 
-	handleFileInput(target: any, input: any): void{
+	handleFileInput(target: any, input: any): void {
 		this.attachments[input.name] = target.files.item(0);
 	}
 }
